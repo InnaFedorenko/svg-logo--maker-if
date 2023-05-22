c4d1719ff80abe977f2945ba4668f7d30a0e1b23
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-05-22 15:24:15 -0400

    added file generation

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-// Connect node module to work with questions
-const inquirer = require('inquirer');
-// Connect node module to work with files
-const fs = require('fs');
+/*
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+*/
+
+// // Connect node module to work with questions
+// const inquirer = require('inquirer');
+// // Connect node module to work with files
+// const fs = require('fs');
 // Connect module to work with date
-const moment = require('moment');
+// const moment = require('moment');
 // Connect my module to generate readme data 
-const generateContent = require('./lib/generateSVG');
+const generateSVG = require('./lib/generateSVG');
 
 // Function to initialize app
 function init() {
-    console.log(`Welcome to the SVG generator!\nPlease follow the instructions:`);
+    console.log('\x1b[33m%s\x1b[0m',`Welcome to the SVG generator!\nPlease follow the instructions:`);
+    generateSVG.renderSVG();
     // console.log(renerateMarkdown.renderLicenseBadge('MIT'))
     //askQuestions(questions);
 }
